4105c1de4ca0b04fb436148894521857db213f83
Franco Quevedo <francoq96@gmail.com>
2023-09-14 00:34:34 -0300

    refactor: reorganizado backend de teleco

--- packages/teleco/rehacer.sh
+++
@@ -1,8 +0,0 @@
-echo "-> Removing container..."
-docker compose -v -f docker-compose.dev.yml down
-echo "-> Removing volume..."
-docker volume rm teleco-db
-echo "-> Removing image..."
-docker rmi teleco-server
-echo "-> Running..."
-docker compose -f docker-compose.dev.yml up
